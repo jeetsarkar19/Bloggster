7534837ca3440298c6521d5d1d1d1bb2e42869fb
Jeet Sarkar <75967415+jeetsarkar19@users.noreply.github.com>
2021-09-05 21:53:50 +0530

    Update settings.py

--- bloggster/bloggster/settings.py
+++ bloggster/bloggster/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wi87%(9lb+=@i_c-gwa(y!(woj(i)1%c_g=t)utfw*htrmm7o5'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
